4a40b554f4b43f4d43ea89f8f4c55aff2d9c0ff2
Jan-Hendrik Willms <tleilax+github@gmail.com>
2014-11-26 13:26:47 +0100

    widget: show message if no articles are available

--- views/widget/index.php
+++ views/widget/index.php
@@ -1,10 +1,14 @@
 <section class="contentbox schwarzesbrett-widget">
     <section>
+    <? if (empty($articles)): ?>
+        <p style="text-align: center;"><?= _('Momentan liegen keine Anzeigen vor.') ?></p>
+    <? else: ?>
         <ul class="sb-articles">
         <? foreach ($articles as $article): ?>
             <?= $this->render_partial('article-li.php', compact('article')) ?>
         <? endforeach; ?>
         </ul>
+    <? endif; ?>
     </section>
 </section>
 

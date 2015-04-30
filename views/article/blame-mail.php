a7bb009b98239ffa900a8f9a399df74f196b2236
Jan-Hendrik Willms <tleilax+github@gmail.com>
2015-04-30 14:34:06 +0200

    include emails of reporter and owner of article in blame mail, fixes #10

--- views/article/blame-mail.php
+++ views/article/blame-mail.php
@@ -3,8 +3,8 @@
 <?= _('Titel') ?>: <?= $article->titel . PHP_EOL ?>
 <?= _('Text') ?>: <?= $article->beschreibung . PHP_EOL ?>
 <?= _('ID') ?>: <?= $article->id . PHP_EOL ?>
-<?= _('Autor') ?>: <?= $article->user->getFullname() ?> (<?= $article->user->username ?>)
-<?= _('Gemeldet von') ?>: <?= $GLOBALS['user']->getFullname() ?> (<?= $GLOBALS['user']->username ?>)
+<?= _('Autor') ?>: <?= $article->user->getFullname() ?> (<?= $article->user->username ?>, <?= $article->user->email ?>)
+<?= _('Gemeldet von') ?>: <?= $GLOBALS['user']->getFullname() ?> (<?= $GLOBALS['user']->username ?>, <?= $GLOBALS['user']->email ?>)
 <?= _('Grund') ?>: <?= $reason . PHP_EOL ?>
 <?= _('Anzeige löschen') ?>: <?= $controller->absolute_url_for('article/delete/' . $article->id) . PHP_EOL ?>
 <?= _('Anzeige bearbeiten') ?>: <?= $controller->absolute_url_for('article/edit/' . $article->id) . PHP_EOL ?>

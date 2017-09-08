1902b8509ba94581cc799110a2239c1a7c4bf690
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-09-08 16:31:48 +0200

    adjust translations, fixes #60

--- classes/Controller.php
+++ classes/Controller.php
@@ -13,7 +13,7 @@ use SearchWidget;
 use SelectElement;
 use SelectWidget;
 use Sidebar;
-use UOL\StudipController;
+use StudipController;
 use Trails_Flash;
 use URLHelper;
 
@@ -22,6 +22,53 @@ class Controller extends StudipController
     protected $allow_nobody = false;
     protected $utf8decode_xhr = true;
     protected $temp_storage;
+
+    /**
+     * Constructs the controller and provide translations methods.
+     *
+     * @param object $dispatcher
+     * @see https://stackoverflow.com/a/12583603/982902 if you need to overwrite
+     *      the constructor of the controller
+     */
+    public function __construct($dispatcher)
+    {
+        parent::__construct($dispatcher);
+
+        $this->plugin = $dispatcher->current_plugin;
+
+        // Localization
+        $this->_ = function ($string) use ($dispatcher) {
+            return call_user_func_array(
+                [$dispatcher->current_plugin, '_'],
+                func_get_args()
+            );
+        };
+
+        $this->_n = function ($string0, $tring1, $n) use ($dispatcher) {
+            return call_user_func_array(
+                [$dispatcher->plugin, '_n'],
+                func_get_args()
+            );
+        };
+    }
+
+    /**
+     * Intercepts all non-resolvable method calls in order to correctly handle
+     * calls to _ and _n.
+     *
+     * @param string $method
+     * @param array  $arguments
+     * @return mixed
+     * @throws RuntimeException when method is not found
+     */
+    public function __call($method, $arguments)
+    {
+        $variables = get_object_vars($this);
+        if (isset($variables[$method]) && is_callable($variables[$method])) {
+            return call_user_func_array($variables[$method], $arguments);
+        }
+        throw new RuntimeException("Method {$method} does not exist");
+    }
 
     public function before_filter(&$action, &$args)
     {

ac29f2a972fffef7189c93a987c36a0604b82066
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-11-16 15:06:51 +0100

    fix encodings, re #67

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -13,7 +13,7 @@ abstract class Plugin extends StudIPPlugin
         parent::__construct();
 
         bindtextdomain(static::GETTEXT_DOMAIN, $this->getPluginPath() . '/locale');
-        bind_textdomain_codeset(static::GETTEXT_DOMAIN, 'ISO-8859-1');
+        bind_textdomain_codeset(static::GETTEXT_DOMAIN, 'UTF-8');
     }
 
     /**

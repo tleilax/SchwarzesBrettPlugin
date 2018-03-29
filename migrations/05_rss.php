57f4d3f1164e733008eed56ca34ace99b24843e4
Jan-Hendrik Willms <tleilax+github@gmail.com>
2018-03-29 10:36:00 +0200

    fix migrations, fixes #75

--- migrations/05_rss.php
+++ migrations/05_rss.php
@@ -9,12 +9,12 @@ class Rss extends Migration
     public function up ()
     {
         $db = DBManager::get();
-        $db->exec("ALTER IGNORE TABLE `sb_artikel` ADD `publishable` tinyint(2) NOT NULL default '0'");
+        $db->exec("ALTER TABLE `sb_artikel` ADD `publishable` tinyint(2) NOT NULL default '0'");
     }
 
     public function down ()
     {
         $db = DBManager::get();
-        $db->exec("ALTER IGNORE TABLE `sb_artikel` DROP `publishable`");
+        $db->exec("ALTER TABLE `sb_artikel` DROP `publishable`");
     }
 }

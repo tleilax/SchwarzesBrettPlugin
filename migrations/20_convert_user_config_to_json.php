c26c1381383ebf860f77c68f14a45f78547c0b29
Jan-Hendrik Willms <tleilax+github@gmail.com>
2018-07-05 17:50:46 +0200

    fix migration, fixes #81

--- migrations/20_convert_user_config_to_json.php
+++ migrations/20_convert_user_config_to_json.php
@@ -3,60 +3,116 @@ class ConvertUserConfigToJson extends Migration
 {
     public function up()
     {
-        $query = "SELECT `userconfig_id`, `value`
-                  FROM `user_config`
-                  WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
-        $statement = DBManager::get()->query($query);
-        $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
+        if (class_exists('StudipVersion') && StudipVersion::newerThan('4.1')) {
+            $query = "SELECT `range_id`, `value`
+                      FROM `config_values`
+                      WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
+            $statement = DBManager::get()->query($query);
+            $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
 
-        $query = "UPDATE `user_config`
-                  SET `value` = :value
-                  WHERE `userconfig_id` = :id";
-        $statement = DBManager::get()->prepare($query);
+            $query = "UPDATE `config_values`
+                      SET `value` = :value
+                      WHERE `range_id` = :id";
+            $statement = DBManager::get()->prepare($query);
 
-        foreach ($data as $id => $value) {
-            $value = unserialize($value);
-            $value = json_encode($value);
+            foreach ($data as $id => $value) {
+                $value = unserialize($value);
+                $value = json_encode($value);
 
-            $statement->bindValue(':id', $id);
-            $statement->bindValue(':value', $value);
+                $statement->bindValue(':id', $id);
+                $statement->bindValue(':value', $value);
+                $statement->execute();
+            }
+
+            $query = "INSERT IGNORE INTO `config` (
+                        `field`, `value`, `is_default`,
+                        `type`, `range`, `description`, `comment`,
+                        `mkdate`, `chdate`
+                      ) VALUES (
+                        'SCHWARZESBRETT_WIDGET_SETTINGS', '[]', 1,
+                        'array', 'user', 'Einstellungen für das Schwarze Brett-Widget', '',
+                        UNIX_TIMESTAMP(), UNIX_TIMESTAMP()
+                      )";
+            DBManager::get()->exec($query);
+
+        } else {
+            $query = "SELECT `userconfig_id`, `value`
+                      FROM `user_config`
+                      WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
+            $statement = DBManager::get()->query($query);
+            $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
+
+            $query = "UPDATE `user_config`
+                      SET `value` = :value
+                      WHERE `userconfig_id` = :id";
+            $statement = DBManager::get()->prepare($query);
+
+            foreach ($data as $id => $value) {
+                $value = unserialize($value);
+                $value = json_encode($value);
+
+                $statement->bindValue(':id', $id);
+                $statement->bindValue(':value', $value);
+                $statement->execute();
+            }
+
+            $query = "INSERT IGNORE INTO `config` (
+                        `config_id`, `field`, `value`, `is_default`,
+                        `type`, `range`, `description`, `comment`,
+                        `mkdate`, `chdate`
+                      ) VALUES (
+                        :id, 'SCHWARZESBRETT_WIDGET_SETTINGS', '[]', 1,
+                        'array', 'user', 'Einstellungen für das Schwarze Brett-Widget', '',
+                        UNIX_TIMESTAMP(), UNIX_TIMESTAMP()
+                      )";
+            $statement = DBManager::get()->prepare($query);
+            $statement->bindValue(':id', md5('SCHWARZESBRETT_WIDGET_SETTINGS'));
             $statement->execute();
         }
-
-        $query = "INSERT IGNORE INTO `config` (
-                    `config_id`, `field`, `value`, `is_default`,
-                    `type`, `range`, `description`, `comment`,
-                    `mkdate`, `chdate`
-                  ) VALUES (
-                    :id, 'SCHWARZESBRETT_WIDGET_SETTINGS', '[]', 1,
-                    'array', 'user', 'Einstellungen für das Schwarze Brett-Widget', '',
-                    UNIX_TIMESTAMP(), UNIX_TIMESTAMP()
-                  )";
-        $statement = DBManager::get()->prepare($query);
-        $statement->bindValue(':id', md5('SCHWARZESBRETT_WIDGET_SETTINGS'));
-        $statement->execute();
     }
 
     public function down()
     {
-        $query = "SELECT `userconfig_id`, `value`
-                  FROM `user_config`
-                  WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
-        $statement = DBManager::get()->query($query);
-        $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
+        if (class_exists('StudipVersion') && StudipVersion::newerThan('4.1')) {
+            $query = "SELECT `range_id`, `value`
+                      FROM `config_values`
+                      WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
+            $statement = DBManager::get()->query($query);
+            $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
 
-        $query = "UPDATE `user_config`
-                  SET `value` = :value
-                  WHERE `userconfig_id` = :id";
-        $statement = DBManager::get()->prepare($query);
+            $query = "UPDATE `config_values`
+                      SET `value` = :value
+                      WHERE `range_id` = :id";
+            $statement = DBManager::get()->prepare($query);
 
-        foreach ($data as $id => $value) {
-            $value = json_decode($value);
-            $value = serialize($value);
+            foreach ($data as $id => $value) {
+                $value = json_decode($value);
+                $value = serialize($value);
 
-            $statement->bindValue(':id', $id);
-            $statement->bindValue(':value', $value);
-            $statement->execute();
+                $statement->bindValue(':id', $id);
+                $statement->bindValue(':value', $value);
+                $statement->execute();
+            }
+        } else {
+            $query = "SELECT `userconfig_id`, `value`
+            FROM `user_config`
+            WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
+            $statement = DBManager::get()->query($query);
+            $data = $statement->fetchGrouped(PDO::FETCH_COLUMN);
+
+            $query = "UPDATE `user_config`
+            SET `value` = :value
+            WHERE `userconfig_id` = :id";
+            $statement = DBManager::get()->prepare($query);
+
+            foreach ($data as $id => $value) {
+                $value = json_decode($value);
+                $value = serialize($value);
+
+                $statement->bindValue(':id', $id);
+                $statement->bindValue(':value', $value);
+                $statement->execute();
+            }
         }
 
         $query = "DELETE FROM `config` WHERE `field` = 'SCHWARZESBRETT_WIDGET_SETTINGS'";
